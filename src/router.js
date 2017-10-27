967bdd2dbaeab467f55459a10ebd6c0c7076cf18
Chris Foster <chriswfoster@gmail.com>
2017-10-27 08:26:41 -0500

    Fixed

--- src/router.js
+++ src/router.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
+
+import Cart from './components/Cart/Cart';
+import Details from './components/Details/Details';
 import Landing from './components/Landing/Landing';
 import Shop from './components/Shop/Shop';
-import Details from './components/Details/Details';
-import Cart from './components/Cart/Cart';
-import ThankYou from './components/ThankYou/ThankYou'
+import ThankYou from './components/ThankYou/ThankYou';
 
-
-
-
-
-export default(
-<Switch>
-<Route exact path="/" component={Landing}/>
-<Route path="/shop" component={Shop}/>
-<Route path="/details/:name" component={Details}/>
-<Route path="/cart" component={Cart}/>
-<Route path="/thankyou" component={ThankYou}/>
-   </Switch>
+export default (
+  <Switch>
+    <Route component={ Landing } exact path="/" />
+    <Route component={ Shop } path="/shop" />
+    <Route component={ Details } path="/details/:name" />
+    <Route component={ Cart } path="/cart" />
+    <Route component={ ThankYou } path="/thank-you" />
+  </Switch>
 )
